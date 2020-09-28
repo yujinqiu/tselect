b44daeaf823d7acd2511c0d7cd409cb7e018b964
yujinqiu <yujinqiu@chengfayun.com>
2020-09-28 10:13:47 +0800

    Support select multi files from CLI and casual path

---
+++ tselect/main.swift
@@ -0,0 +1,82 @@
+//
+//  main.swift
+//  select
+//
+//  Created by knight on 9/28/20.
+//
+
+import Foundation
+import AppKit
+import ArgumentParser
+
+extension String {
+    func trimPrefix(_ prefix: String) -> String {
+        guard self.hasPrefix(prefix) else { return self}
+        return String(self.dropFirst(prefix.count))
+    }
+}
+
+struct Directory {
+    static func CurrentWorkingDirectory()-> String {
+        return  FileManager.default.currentDirectoryPath
+    }
+}
+
+struct Path {
+    static let seperator = "/"
+    static let relativePrefix = "./"
+    let fm = FileManager.default
+    var path: String
+    var cwd: String
+    
+    init(path: String) {
+        self.path = path
+        self.cwd = Directory.CurrentWorkingDirectory()
+    }
+    
+    
+    var isAbsolute: Bool {
+        return path.hasPrefix(Path.seperator)
+    }
+    
+    var isRelative: Bool {
+        return !isAbsolute
+    }
+    
+    var absPath: String {
+        if isAbsolute {
+            return path
+        }
+        let path =  "\(self.cwd)/\(self.path)"
+        return path
+    }
+    
+    var exists: Bool {
+        return fm.fileExists(atPath: self.absPath)
+    }
+}
+
+struct Target: ParsableCommand {
+    @Argument(help: "files that will be selected in Finder.")
+    var files: [String]
+}
+
+let target = Target.parseOrExit()
+var urls:[URL] = []
+
+for file in target.files {
+    let path = Path(path: file)
+    if path.exists {
+        let url = NSURL(fileURLWithPath: path.absPath)
+        // standardizing path: transform ../.. to the right path
+        guard let standardizedPaht = url.standardizingPath else {
+            continue
+        }
+        urls.append(standardizedPaht)
+    }
+}
+
+if urls.count > 0 {
+    NSWorkspace.shared.activateFileViewerSelecting(urls)
+}
+
